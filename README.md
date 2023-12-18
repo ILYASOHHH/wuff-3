879dde381e78dad17f56a4a2047721827676435a
ILYASOHHH <153507195+ILYASOHHH@users.noreply.github.com>
2023-12-18 11:50:14 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Diese Seite bei [https://ilyasohhh.github.io/wuff-3/](https://ilyasohhh.github.io/wuff-3/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/ilyasohhh/wuff-3** suchen und importieren
+
+## Dieses Projekt bearbeiten
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/ilyasohhh/wuff-3** ein und klicke auf Importieren
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        # . # . #
+        `)
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # . # . #
+        . . . . .
+        `)
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.setLedColor(0xff0000)
+})
